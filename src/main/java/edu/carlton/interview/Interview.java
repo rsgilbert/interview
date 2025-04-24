e0127cc2c611116895cbf4cbf44cfaddc69ee95f
Gilbert Ssenyonjo <SSG@uga.hrpsolutions.com>
2025-04-24 03:39:59 +0100

    implemented a gui for adding two numbers

--- src/main/java/edu/carlton/interview/Interview.java
+++ src/main/java/edu/carlton/interview/Interview.java
@@ -4,6 +4,10 @@
 
 package edu.carlton.interview;
 
+
+import javax.swing.JOptionPane;
+
+
 /**
  *
  * @author SSG
@@ -12,5 +16,16 @@ public class Interview {
 
     public static void main(String[] args) {
         System.out.println("Hello Uganda!");
+        
+        String fn = JOptionPane.showInputDialog("Enter first number");
+        String sn = JOptionPane.showInputDialog("Enter second number");
+        
+        int num1 = Integer.parseInt(fn);
+        int num2 = Integer.parseInt(sn);
+        
+        int sum = num1 + num2;
+        
+        JOptionPane.showMessageDialog(null, "The answer is " + sum, "SUM", JOptionPane.PLAIN_MESSAGE);
+        
     }
 }
