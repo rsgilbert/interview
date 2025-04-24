a80c61b0f91b2bad37b7474ea046df05bddc9ab3
Gilbert Ssenyonjo <SSG@uga.hrpsolutions.com>
2025-04-24 03:30:56 +0100

    Initial commit. Created java project

---
+++ src/main/java/edu/carlton/interview/Interview.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package edu.carlton.interview;
+
+/**
+ *
+ * @author SSG
+ */
+public class Interview {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Uganda!");
+    }
+}
