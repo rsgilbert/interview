0382ca1aa657329455b3c9fb0030a71cc64e4bfb
rsgilbert <gssenyonjo@gmail.com>
2025-04-27 21:00:34 +0300

    Added form for shortness of breath interview

---
+++ src/main/java/edu/carlton/interview/ShortnessOfBreathInterviewManager.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.carlton.interview;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author SSG
+ */
+public class ShortnessOfBreathInterviewManager {
+    
+    public static List<Question> getQuestions() {
+        var questions = new ArrayList<Question>();
+        
+        
+        return questions;
+        
+    }
+    
+}
